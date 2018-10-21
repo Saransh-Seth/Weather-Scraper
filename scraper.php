1aabbe8421f1d23466edc22f9e69c1b45874fa2d
Saransh Seth <saranshseth93@yahoo.com>
2018-10-21 16:15:29 +1100

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -3,5 +3,5 @@
 	$city=str_replace(" ","",$city);
 	$contents = file_get_contents("http://www.weather-forecast.com/locations/".$city."/forecasts/latest");
 	preg_match('/<h2>10 Day Melbourne Weather <\/h2>(7&ndash;10 days)<\/span><p class="b-forecast__table-description-content"><span class="phrase">(.*?)</s',$contents,$matches);
-	echo $matches[1];
+	echo $matches;
 ?>
