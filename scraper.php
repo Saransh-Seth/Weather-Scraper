78a9c6c62884e7aca98691cdfc5df9b081bfa866
Saransh Seth <saranshseth93@yahoo.com>
2018-10-21 13:58:06 +1100

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -2,7 +2,6 @@
 	$city=$_GET['city'];
 	$city=str_replace(" ","",$city);
 	$contents = file_get_contents("http://www.weather-forecast.com/locations/".$city."/forecasts/latest");
-	echo $contents;
-	preg_match('/3 Day Weather Forecast Summary:<\/b><span class="read-more-small"><span class="read-more-content"> <span class="phrase">(.*?)</s',$contents,$matches);
+	preg_match('/10 Day '.$city.' Weather <\/h2>(7–10 days)<\/span><p class="b-forecast__table-description-content"><span class="phrase">(.*?)</s',$contents,$matches);
 	echo $matches[1];
 ?>
